2fae6c2100604c7b9984d83047220e469ad1105d
Francesco Melensi <melensi.francesco@gmail.com>
2024-05-02 23:32:30 +0200

    first real commit

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+}
